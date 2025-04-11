4e87348ec69d65119a66f908c9d2e4f94fab7788
Gabriel Mania <maniagp@proton.me>
2025-04-11 23:42:31 +0800

    perf: Implemented a flash message after creating a snippet
    Flash message is stored in the session manager

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -48,7 +48,10 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 }
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
-	return &templateData{CurrentYear: time.Now().Year()}
+	return &templateData{
+		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+	}
 }
 
 func (app *application) decodePostForm(r *http.Request, dst any) error {
